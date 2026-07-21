e7077e6568acc32322d1bf7b93fc7ee5f0836673
CodingSphinx <codingsphinx@duck.com>
2026-07-21 18:19:36 -0400

    Refactor RootLayout to include AuthGate and remove settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,26 @@
-import AuthGate from '@/providers/AuthGate';
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import AuthGate from '@/providers/AuthGate';
 import { SessionProvider } from '@/providers/SessionProvider';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <SessionProvider>
-  <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-    <AuthGate />
-    <StatusBar style="auto" />
-  </ThemeProvider>
-</SessionProvider>
+      <ThemeProvider
+        value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+      >
+        <AuthGate />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
